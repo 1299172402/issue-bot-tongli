d603391a9df161a5443c41bf65da9f717efc3b5b
之雨 <1299172402@qq.com>
2022-08-22 20:44:23 +0800

    Update trigger.py
    - add run id comment

--- trigger.py
+++ trigger.py
@@ -4,36 +4,46 @@ import requests
 
 GHP_TONGLI = os.environ["GHP_TONGLI"]
 WORKFLOW_URL = os.environ["WORKFLOW_URL"]
+ACTION_URL = os.environ["ACTION_URL"]
+
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"token {GHP_TONGLI}"
+}
 
 def main():
     url = 'https://api.github.com/repos/1299172402/tongli-new/issues'
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"token {GHP_TONGLI}"
-    }
     res = requests.get(url, headers=headers).json()
     if res[0]['labels'][0]['name'] == 'Runner':
         params = res[0]['body']
         number = res[0]['number']
 
     params = json.loads(params)
+    print(f'[info] params: {params}')
 
     url = WORKFLOW_URL
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"token {GHP_TONGLI}"
-    }
     data = {
-        "ref":"main",
+        "ref": "main",
         "inputs": {
             "type": params['type'],
             "bookid": params['bookid'],
             "isSerial": params['isSerial'],
             "RefreshToken": params['RefreshToken'],
-            "issue_number": number,
+            "issue_number": str(number)
         }
     }
-    requests.post(url, headers=headers, data=data)
+    res = requests.post(url, headers=headers, data=json.dumps(data))
+    print(f'[info] workflow: {res}')
+
+    url = ACTION_URL
+    res = requests.get(url, headers=headers).json()
+    run_id = res['workflow_runs'][0]['id']
+    print(f'[info] run_id: {run_id}')
+
+    url = f'https://api.github.com/repos/1299172402/tongli-new/issues/{str(number)}/comments'
+    data = {"body": f"Your run id is ${run_id}$. We'll let you know when it's done."}
+    requests.post(url, data=json.dumps(data))
+    print(f'[info] comment: start download')
 
 if __name__ == '__main__':
     main()
