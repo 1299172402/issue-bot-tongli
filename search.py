1c8d692f20659e2dba76ae31176002e80f6db2b1
之雨 <1299172402@qq.com>
2022-08-23 12:55:15 +0800

    update: search

---
+++ search.py
@@ -0,0 +1,68 @@
+import requests
+
+GHP_TONGLI = os.environ["GHP_TONGLI"]
+NUMBER = os.environ["NUMBER"]
+
+BASE_API_URL = 'https://api.github.com'
+owner = '1299172402'
+repo = 'tongli-new'
+issue_number = NUMBER
+
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"token {GHP_TONGLI}"
+}
+
+def comment(message):
+    url = f'{BASE_API_URL}/repos/{owner}/{repo}/issues/{str(issue_number)}/comments'
+    data = {"body": f"[bot] {message}"}
+    requests.post(url, headers=headers, data=json.dumps(data))
+    print(f'[info] comment: {message}')
+
+def func(BookGroupID, Title, CoverURL, Author, IsSerial):
+    url = f'https://api.tongli.tw/Book/BookVol/{BookGroupID}?isSerial={IsSerial}'
+    res = requests.get(url).json()
+    BookID = res[-1]['BookID']
+
+    url = f'https://api.tongli.tw/Book?bookID={BookID}'
+    res = requests.get(url).json()
+    Introduction = res['Introduction']
+    OnShelfDate = res['OnShelfDate']
+    RankStar = res['Rank']['RankStar']
+    Vol = res['Vol']
+
+    text = f'''
+![Cover]({CoverURL})
+{Title} © {Author} 著
+{''.join('⭐' for _ in range(RankStar))}
+---
+{Introduction}
+
+- 最新话 {Vol}
+- 上架时间 {OnShelfDate}
+    '''
+    comment(text)
+
+def main():
+    url = f'{BASE_API_URL}/repos/{owner}/{repo}/issues/{issue_number}'
+    res = requests.get(url, headers=headers).json()
+    SearchStr = res['body']
+
+    url = 'https://api.tongli.tw/Search'
+    data = {
+        "SearchStr": SearchStr
+    }
+    res = requests.post(url, data).json()
+    if res:
+        for item in res:
+            BookGroupID = item['BookGroupID']
+            Title = item['Title']
+            CoverURL = item['CoverURL']
+            Author = item['Author']
+            IsSerial = item['IsSerial']
+            func(BookGroupID, Title, CoverURL, Author, IsSerial)
+    else:
+        comment("无任何搜索结果")
+
+if __name__ == '__main__':
+    main()
